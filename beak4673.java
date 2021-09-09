182b4ddf0582f642d41d3f836f9ad65b39f79a67
3kk-1st <67462317+3kk-1st@users.noreply.github.com>
2021-09-09 15:21:32 +0900

    main

---
+++ beak4673.java
@@ -0,0 +1,5 @@
+public class beak4673 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ grammers_print.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class grammers_print {
+    public static void main() {
+
+        
+    }
+    
+}
